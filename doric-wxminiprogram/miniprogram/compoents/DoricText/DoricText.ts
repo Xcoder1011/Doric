b3e765e79ae86bef01ca6228ac5356c15a0e35e3
pengfei.zhou <pengfeizhou@foxmail.com>
2021-10-26 11:28:00 +0800

    Add some project files

---
+++ doric-wxminiprogram/miniprogram/compoents/DoricText/DoricText.ts
@@ -0,0 +1,32 @@
+// compoents/DoricText/DoricText.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    doricModel: {
+      type: Object
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+  },
+  lifetimes:{
+    attached :function(){
+      console.log(this.properties)
+      this.setData({
+        doricModel:this.properties.doricModel
+      })
+    }
+  }
+})

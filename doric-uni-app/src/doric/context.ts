b46506aca76ae70d86123b391e4c647f25ac0470
王劲鹏 <jingpeng.wang@outlook.com>
2021-11-01 14:41:02 +0800

    format

--- doric-uni-app/src/doric/context.ts
+++ doric-uni-app/src/doric/context.ts
@@ -1,71 +1,91 @@
-import { BridgeContext, Panel, ClassType, registerViewTreeObserver } from "doric";
+import {
+  BridgeContext,
+  Panel,
+  ClassType,
+  registerViewTreeObserver,
+} from "doric";
 
-const gContexts: Map<string, Context> = new Map
+const gContexts: Map<string, Context> = new Map();
 
 const updator = () => {
-  gContexts.forEach(context => {
-    context.hookAfter?.()
-  })
-  updatorTask = undefined
-}
+  gContexts.forEach((context) => {
+    context.hookAfter?.();
+  });
+  updatorTask = undefined;
+};
 
-let updatorTask: number | undefined = undefined
+let updatorTask: number | undefined = undefined;
 
 registerViewTreeObserver(() => {
   if (updatorTask !== undefined) {
-    return
+    return;
   }
-  updatorTask = setTimeout(updator, 0)
-})
+  updatorTask = setTimeout(updator, 0);
+});
 
-export function callResponse(contextId: string, idList: string[], funcId: string, args?: any) {
+export function callResponse(
+  contextId: string,
+  idList: string[],
+  funcId: string,
+  args?: any
+) {
   return callEntityMethod(contextId, "__response__", idList, funcId, args);
 }
 
-export function callEntityMethod(contextId: string, methodName: string, ..._: any) {
-  const context = gContexts.get(contextId)
+export function callEntityMethod(
+  contextId: string,
+  methodName: string,
+  ..._: any
+) {
+  const context = gContexts.get(contextId);
   if (context === undefined) {
-    console.error(`Cannot find context for context id:${contextId}`)
-    return
+    console.error(`Cannot find context for context id:${contextId}`);
+    return;
   }
   if (context.entity === undefined) {
-    console.error(`Cannot find holder for context id:${contextId}`)
-    return
+    console.error(`Cannot find holder for context id:${contextId}`);
+    return;
   }
 
   if (Reflect.has(context.entity, methodName)) {
-    const argumentsList: any = []
+    const argumentsList: any = [];
     for (let i = 2; i < arguments.length; i++) {
-      argumentsList.push(arguments[i])
+      argumentsList.push(arguments[i]);
     }
-    return Reflect.apply(Reflect.get(context.entity, methodName), context.entity, argumentsList)
+    return Reflect.apply(
+      Reflect.get(context.entity, methodName),
+      context.entity,
+      argumentsList
+    );
   } else {
-    console.error(`Cannot find method for context id:${contextId},method name is:${methodName}`)
+    console.error(
+      `Cannot find method for context id:${contextId},method name is:${methodName}`
+    );
   }
 }
 
 export function createContext(contextId: string, clz: ClassType<Panel>) {
-  const context = new Context(contextId, clz)
-  gContexts.set(contextId, context)
+  const context = new Context(contextId, clz);
+  gContexts.set(contextId, context);
   return context;
 }
 
 export function destroyContext(contextId: string) {
-  gContexts.delete(contextId)
+  gContexts.delete(contextId);
 }
 export function obtainContext(contextId: string) {
-  return gContexts.get(contextId)
+  return gContexts.get(contextId);
 }
 
 export class Context implements BridgeContext {
   id: string;
   entity: Panel;
-  hookAfter: Function | undefined
+  hookAfter: Function | undefined;
 
   constructor(id: string, clz: ClassType<Panel>) {
     this.id = id;
-    this.entity = new clz
-    this.entity.context = this
+    this.entity = new clz();
+    this.entity.context = this;
   }
   callNative(namespace: string, method: string, args?: any): Promise<any> {
     throw new Error("Method not implemented.");
@@ -76,5 +96,4 @@ export class Context implements BridgeContext {
   removeFuncById(funcId: string): void {
     throw new Error("Method not implemented.");
   }
-
 }
